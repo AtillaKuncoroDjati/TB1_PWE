8a00e792df8f2d1a136a754fa41c6767b9cdef2b
AtillaKuncoroDjati <ella300304@gmail.com>
2024-10-22 01:47:33 +0700

    Add files via upload
    Tugas Besar 1_Pemogramman Web Enterprise

---
+++ storage/framework/views/de9ed5cb19db59b3de10fd86d74aca30.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Forbidden')); ?>
+<?php $__env->startSection('code', '403'); ?>
+<?php $__env->startSection('message', __($exception->getMessage() ?: 'Forbidden')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\ella3\OneDrive\Dokumen\UNIVERSITAS MERCU BUANA\SEMESTER 5\PWE\Atilla Kuncoro Djati\atillakd\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/403.blade.php ENDPATH**/ ?>
